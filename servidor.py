dbc0ea618b1da22cba41b4b6c29de521aa3fe33c
Tiago Rodrigo Kepe <tiagokepe@gmail.com>
2011-05-25 14:57:50 -0300

    Implementação inicial do servidor

---
+++ servidor.py
@@ -0,0 +1,24 @@
+from socket import *
+
+from sys import *
+
+if len(argv) != 2:
+    print "Uso correto: servidor <porta>"
+    exit(1)
+
+try:
+    sock_descr = socket(AF_INET6, SOCK_STREAM)
+except socket.error:
+    print "Erro ao criar o socket"
+    exit(1)
+
+host = "localhost"
+port = int(argv[1])
+addr = (host,port)
+
+sock_descr.bind(addr)
+
+sock_descr.close()
+
+#print "Porta: " + porta
+
